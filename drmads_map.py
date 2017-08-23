6010ae16d84c7d29ef090ed0eacc1ed6ca7d8016
juggernaughton <27182818+juggernaughton@users.noreply.github.com>
2017-08-22 23:11:17 -0400

    On The Move!!!
    Corrected issues that would not transport user to the next designated room. Next action item is to implement storyline.

--- drmads_map.py
+++ drmads_map.py
@@ -2,14 +2,22 @@ from sys import exit
 from random import randint
 
 class Scene(object):
-	print "This class is not configured yet. Implement as subclass."
-	exit(1)
+	def enter(self):
+		print "This class is not configured yet. Implement as subclass. Ya know?"
+		exit(1)
 
 class MainRoom(Scene):
 	def enter(self):
 		print "You are in the main room"
-		return 'main_room'
-
+		print "You have two choices: 'stay' in the main room, or... 'run' to your left."
+		action = raw_input("> ")
+		
+		if action == 'stay':
+			print "You chose to stay in the main room like a little bitch!"
+			return 'main_room'
+		elif action == 'run':
+			print "You chose to run like the little bitch you are."
+			return 'multiplication'
 class Multiplication(Scene):
 	def enter(self):
 		print "You are in the Multiplication Room."
@@ -35,24 +43,22 @@ class Engine(object):
 
 	def __init__(self, start_scene):
 		self.first_scene = start_scene
-		print "The first scene is %s ", self.first_scene
-	
-	def get_scene_name(self, scene_name):
-		scene_name = Engine.scenes.get(self.first_scene)
+		print "Engine __init__ has start_scene", start_scene
+		print "\n\n"
 	
  	def play(self):
- 		current_scene = self.first_scene.get_scene_name()
+ 		current_scene = Engine.scenes.get(self.first_scene)
+ 		print "current_scene is: ", current_scene
+
  		
  		while True:
  			print "\n\t\t-----------------"
  			next_scene_name = current_scene.enter()
- 			current_scene = self.first_scene.get_scene_name(next_scene_name) 
-
+ 			next_scene = Engine.scenes.get(next_scene_name)
+  			
 	
 startengine = Engine('main_room')
 
-print "startengine has %s from Engine", startengine
-print self.first_scene
 startengine.play()
  
 
